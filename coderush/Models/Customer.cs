ffb1078e6a0573de068f61b6b4f825fd53d5903e
iamrames <addictedtomig@gmail.com>
2021-01-17 21:34:39 +0545

    first commit

---
+++ coderush/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coderush.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+        [Required]
+        public string CustomerNameLocLang { get; set; }
+        // [Display(Name = "Customer Type")]
+        // public int CustomerTypeId { get; set; }
+        [Display(Name = "Street Address")]
+        public string Address { get; set; }
+        // public string City { get; set; }
+        // public string State { get; set; }
+        // [Display(Name = "Zip Code")]
+        // public string ZipCode { get; set; }
+        public string Phone { get; set; }
+
+        [Display(Name = "Pan No.")]
+        public string PanNo { get; set; }
+        // public string Email { get; set; }
+        // [Display(Name = "Contact Person")]
+        // public string ContactPerson { get; set; }
+    }
+}
